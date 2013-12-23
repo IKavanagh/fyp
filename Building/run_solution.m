0a1bb957c76b231b5cfb78b9264da7a2fcd563bc
Ian Kavanagh <kavanagh.ian4@gmail.com>
2013-12-23 18:12:30 +0000

    Added movies of real part of total electric field propagating through area

--- Building/run_solution.m
+++ Building/run_solution.m
@@ -36,3 +36,35 @@ if 1 == 1 % Incident field
     ylabel('Distance (m)');
     title('Plot of Incident Electric Field in Building');
 end
+
+% Movie
+if 1 == 1
+    h = figure;
+    
+    start_time = 0;
+    end_time = 10;
+    step_time = (end_time - start_time) / 200;
+    
+    t = start_time:step_time:end_time;
+    
+    X = real(pos);
+    Y = imag(pos);
+    
+    for i = 1:length(t)
+        Et = E_total + 1i*E_total;
+        wave = exp(1i*t(i));
+        
+        Z = Et*wave;
+
+        surf(X, Y, real(Z));
+        shading interp;
+        view(2);
+        xlabel('Distance (m)');
+        ylabel('Distance (m)');
+        title('Plot of Total Electric Field Interacting on Cylindrical Scatterer');
+        
+        movie_frames(i) = getframe(gcf);
+    end
+    
+    movie2avi(movie_frames, 'building.avi');
+end
--- Cylindrical Scatterer/CG-FFT Reduced/verify_solutions.m
+++ Cylindrical Scatterer/CG-FFT Reduced/verify_solutions.m
@@ -66,3 +66,35 @@ if 1 == 1
     %filename = sprintf('p_electric_field_r%0.f', radius);
     %hgsave(h, filename);
 end
+
+% Movie
+if 1 == 1
+    h = figure;
+    
+    start_time = 0;
+    end_time = 10;
+    step_time = (end_time - start_time) / 200;
+    
+    t = start_time:step_time:end_time;
+    
+    X = real(pos);
+    Y = imag(pos);
+    
+    for i = 1:length(t)
+        Et = E_total_vefie_real + 1i*E_total_vefie_imag;
+        wave = exp(1i*t(i));
+        
+        Z = Et*wave;
+
+        surf(X, Y, real(Z));
+        shading interp;
+        view(2);
+        xlabel('Distance (m)');
+        ylabel('Distance (m)');
+        title('Plot of Total Electric Field Interacting on Cylindrical Scatterer');
+        
+        movie_frames(i) = getframe(gcf);
+    end
+    
+    movie2avi(movie_frames, 'scatterer.avi');
+end
