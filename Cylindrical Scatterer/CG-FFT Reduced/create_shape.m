3e4a3cd9ed07c8e1fd97a4596bbc4763558df4db
Ian Kavanagh <kavanagh.ian4@gmail.com>
2013-12-23 12:50:28 +0000

    Improved runtime by reducing the number of loops

--- Cylindrical Scatterer/CG-FFT Reduced/create_shape.m
+++ Cylindrical Scatterer/CG-FFT Reduced/create_shape.m
@@ -3,6 +3,8 @@
 tic;
 
 position = zeros(problem_size, 1);
+scatterer = zeros(M, N);
+pos = zeros(M, N);
 
 wave_number(1:problem_size, 1) = k0;
 
@@ -15,11 +17,13 @@ for y = 1:M % y-direction
         
         % Define position as center of cell i.e. (x - 1/2)
         position(position_counter, 1) = start_point + ((x - 0.5)*delta_x + (y - 0.5)*delta_y*1i);
+        pos(y, x) = position(position_counter);
         
         % If cell is within circle its wave number is different to free
         % space
         if (abs(position(position_counter, 1) - centre) <= radius)
            wave_number(position_counter, 1) = kd; 
+           scatterer(y, x) = 1;
         end
     end
 end
@@ -28,18 +32,3 @@ end
 
 t = toc;
 fprintf(1, 'Created shape of width %.4f metres and length %.4f meters and discretised into %.0f cells in %.4f seconds\n', x_side, y_side, problem_size, t);
-
-% Flatten out scatterer so it can be viewed visually
-scatterer = zeros(M, N);
-pos = zeros(M, N);
-for y = 1:M
-    for x = 1:N
-        position_counter = (y - 1)*N + x;
-        
-        pos(y, x) = position(position_counter);
-
-        if (wave_number(position_counter) ~= k0)
-            scatterer(y, x) = 1;
-        end
-    end
-end
